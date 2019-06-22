fe5ee09f5770038fe855186c88f1d36271bb72da
adishthapa <adishthapa@outlook.com>
2019-06-22 12:23:35 -0400

    Added initial site files

---
+++ javascript/app.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyDl_LzzZ5sNq7V0VFm9yEi2aKmmC028emg",
+    authDomain: "train-scheduler-7339f.firebaseapp.com",
+    databaseURL: "https://train-scheduler-7339f.firebaseio.com",
+    projectId: "train-scheduler-7339f",
+    storageBucket: "",
+    messagingSenderId: "306079155504",
+    appId: "1:306079155504:web:476321b9de591b0e"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
