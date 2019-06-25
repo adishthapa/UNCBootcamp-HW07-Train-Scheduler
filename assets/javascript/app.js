c75a388cf1be8a7d193f8dbe846f6948b3b6d3af
adishthapa <adishthapa@outlook.com>
2019-06-25 00:58:20 -0400

    Got basic functionality working, needs firebase

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,3 +11,56 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+function createTrain(train) {
+    var nextArrival = "";
+    var minutesAway = "";
+    var newTrain = $("<tr>");
+
+    var trainTime = moment(train.time, "HH:mm").format("X");
+    var frequency = train.frequency * 60;
+    var currentTime = moment().format("X");
+
+    if (trainTime > currentTime) {
+        var newTime = moment(currentTime, "X").add(frequency, "seconds").format("X");
+        if (newTime >= trainTime) { 
+            nextArrival = trainTime;
+        } else {
+            while(trainTime > currentTime) {
+                trainTime = moment(trainTime, "X").subtract(frequency, "seconds").format("X");
+            }
+            trainTime = moment(trainTime, "X").add(frequency, "seconds").format("X");
+            nextArrival = trainTime;
+        };
+    } else {
+        while (trainTime <= currentTime) {
+            trainTime = moment(trainTime, "X").add(frequency, "seconds").format("X");
+        }
+        nextArrival = trainTime;
+    };
+
+    minutesAway = moment(nextArrival, "X").diff(moment(currentTime, "X"), "minutes");
+
+    if (minutesAway == "0") {
+        minutesAway = "Less than a minute away";
+    };
+
+    var trainName = $("<th>").text(train.name);
+    var trainDestination = $("<td>").text(train.destination);
+    var trainFrequency = $("<td>").text(train.frequency);
+    var trainNextArrival = $("<td>").text(moment(nextArrival, "X").format("HH:mm"));
+    var trainMinutesAway = $("<td>").text(minutesAway);
+
+
+    newTrain.append(trainName).append(trainDestination).append(trainFrequency).append(trainNextArrival).append(trainMinutesAway);
+    $(".train").append(newTrain);
+}
+
+$("#submit").on("click", function() {
+    var train = {
+        name: $("#name-input").val(),
+        destination: $("#destination-input").val(),
+        time: $("#time-input").val(),
+        frequency: $("#frequency-input").val()
+    }
+    createTrain(train);
+});
