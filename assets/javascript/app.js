e4994c2960d377cf57c0eea1237d1518c9af1033
adishthapa <adishthapa@outlook.com>
2019-06-25 01:45:25 -0400

    Added firebase functionalities

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,4 @@
-// Your web app's Firebase configuration
+// Firebase configuration
 var firebaseConfig = {
     apiKey: "AIzaSyDl_LzzZ5sNq7V0VFm9yEi2aKmmC028emg",
     authDomain: "train-scheduler-7339f.firebaseapp.com",
@@ -11,15 +11,23 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+// Create a variable to reference the database.
+var database = firebase.database();
+// Link to Firebase Database for tracking trains
+var trainsRef = database.ref("/trains");
+
+// Function responsible for creating the train's information on the HTML
 function createTrain(train) {
+    // First train time
+    var trainTime = moment(train.time, "HH:mm").format("X");
+    // How often the train comes
+    var frequency = train.frequency * 60;
+    // Current time
+    var currentTime = moment().format("X");
     var nextArrival = "";
     var minutesAway = "";
-    var newTrain = $("<tr>");
 
-    var trainTime = moment(train.time, "HH:mm").format("X");
-    var frequency = train.frequency * 60;
-    var currentTime = moment().format("X");
-
+    // Calculates the new arrival time of the train
     if (trainTime > currentTime) {
         var newTime = moment(currentTime, "X").add(frequency, "seconds").format("X");
         if (newTime >= trainTime) { 
@@ -38,29 +46,59 @@ function createTrain(train) {
         nextArrival = trainTime;
     };
 
+    // Calculates how far the next train is
     minutesAway = moment(nextArrival, "X").diff(moment(currentTime, "X"), "minutes");
-
     if (minutesAway == "0") {
         minutesAway = "Less than a minute away";
     };
 
+    // Writes the following variables to the html
+    var newTrain = $("<tr>");
     var trainName = $("<th>").text(train.name);
     var trainDestination = $("<td>").text(train.destination);
     var trainFrequency = $("<td>").text(train.frequency);
     var trainNextArrival = $("<td>").text(moment(nextArrival, "X").format("HH:mm"));
     var trainMinutesAway = $("<td>").text(minutesAway);
-
-
     newTrain.append(trainName).append(trainDestination).append(trainFrequency).append(trainNextArrival).append(trainMinutesAway);
     $(".train").append(newTrain);
 }
 
+// Checks for the click of the submission button and adds the new train to the firebase
 $("#submit").on("click", function() {
-    var train = {
-        name: $("#name-input").val(),
-        destination: $("#destination-input").val(),
-        time: $("#time-input").val(),
-        frequency: $("#frequency-input").val()
-    }
-    createTrain(train);
+        var name = $("#name-input").val().trim();
+        var destination = $("#destination-input").val().trim();
+        var time = $("#time-input").val().trim();
+        var frequency = $("#frequency-input").val().trim();
+
+        trainsRef.push({
+            name,
+            destination,
+            time,
+            frequency
+        });
+
+        $("#name-input").val("");
+        $("#destination-input").val("");
+        $("#time-input").val("");
+        $("#frequency-input").val("");
+});
+
+// Gets the snapshot of the local data during the page load and subsequen value changes
+trainsRef.on("value", function(snapshot) {
+    $(".train").empty();
+
+    snapshot.forEach((child) => {
+        var train = {
+            name: child.val().name,
+            destination: child.val().destination,
+            time: child.val().time,
+            frequency: child.val().frequency
+        }
+
+        console.log(train.name);
+        console.log(train.destination);
+        console.log(train.time);
+        console.log(train.frequency);
+        createTrain(train);
+    });
 });
